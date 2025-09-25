0ff39759714c3015c8cdde53bb3025462b540394
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-09-25 15:39:01 -0300

    chore: DDL alterada para incluir a coluna name na tabela user

--- src/main/resources/db/ddl.sql
+++ src/main/resources/db/ddl.sql
@@ -1,7 +1,70 @@
 -- DROP SCHEMA public;
 
 CREATE SCHEMA public AUTHORIZATION pg_database_owner;
--- public.companies definição
+
+-- DROP SEQUENCE public.checkin_id_seq;
+
+CREATE SEQUENCE public.checkin_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.companies_id_seq;
+
+CREATE SEQUENCE public.companies_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.events_id_seq;
+
+CREATE SEQUENCE public.events_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.events_users_id_seq;
+
+CREATE SEQUENCE public.events_users_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.feedbacks_id_seq;
+
+CREATE SEQUENCE public.feedbacks_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.permissions_users_id_seq;
+
+CREATE SEQUENCE public.permissions_users_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.users_id_seq;
+
+CREATE SEQUENCE public.users_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;-- public.companies definição
 
 -- Drop table
 
@@ -11,20 +74,6 @@ CREATE TABLE public.companies (
 	id serial4 NOT NULL,
 	"name" varchar(100) NOT NULL,
 	CONSTRAINT companies_pk PRIMARY KEY (id)
-);
-
-
--- public.permissions definição
-
--- Drop table
-
--- DROP TABLE public.permissions;
-
-CREATE TABLE public.permissions (
-	id int4 NOT NULL,
-	description varchar(30) NOT NULL,
-	CONSTRAINT permissions_pk PRIMARY KEY (id),
-	CONSTRAINT permissions_unique UNIQUE (description)
 );
 
 
@@ -45,6 +94,20 @@ CREATE TABLE public.events (
 );
 
 
+-- public.permissions definição
+
+-- Drop table
+
+-- DROP TABLE public.permissions;
+
+CREATE TABLE public.permissions (
+	id int4 NOT NULL,
+	description varchar(30) NOT NULL,
+	CONSTRAINT permissions_pk PRIMARY KEY (id),
+	CONSTRAINT permissions_unique UNIQUE (description)
+);
+
+
 -- public.users definição
 
 -- Drop table
@@ -55,25 +118,26 @@ CREATE TABLE public.users (
 	id serial4 NOT NULL,
 	email varchar(100) NOT NULL,
 	company_id int4 NOT NULL,
+	"name" varchar(60) NOT NULL,
 	CONSTRAINT users_pk PRIMARY KEY (id),
 	CONSTRAINT users_unique UNIQUE (email),
 	CONSTRAINT users_companies_fk FOREIGN KEY (company_id) REFERENCES public.companies(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
--- public.permissions_users definição
+-- public.checkin definição
 
 -- Drop table
 
--- DROP TABLE public.permissions_users;
+-- DROP TABLE public.checkin;
 
-CREATE TABLE public.permissions_users (
+CREATE TABLE public.checkin (
 	id serial4 NOT NULL,
 	user_id int4 NOT NULL,
-	permission_id int4 NOT NULL,
-	CONSTRAINT permissions_users_pk PRIMARY KEY (id),
-	CONSTRAINT permissions_users_permissions_fk FOREIGN KEY (permission_id) REFERENCES public.permissions(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT permissions_users_users_fk FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE ON UPDATE CASCADE
+	event_id int4 NOT NULL,
+	CONSTRAINT checkin_pk PRIMARY KEY (id),
+	CONSTRAINT checkin_events_fk FOREIGN KEY (event_id) REFERENCES public.events(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT checkin_users_fk FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
@@ -110,17 +174,17 @@ CREATE TABLE public.feedbacks (
 );
 
 
--- public.checkin definição
+-- public.permissions_users definição
 
 -- Drop table
 
--- DROP TABLE public.checkin;
+-- DROP TABLE public.permissions_users;
 
-CREATE TABLE public.checkin (
+CREATE TABLE public.permissions_users (
 	id serial4 NOT NULL,
 	user_id int4 NOT NULL,
-	event_id int4 NOT NULL,
-	CONSTRAINT checkin_pk PRIMARY KEY (id),
-	CONSTRAINT checkin_events_fk FOREIGN KEY (event_id) REFERENCES public.events(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT checkin_users_fk FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE ON UPDATE CASCADE
+	permission_id int4 NOT NULL,
+	CONSTRAINT permissions_users_pk PRIMARY KEY (id),
+	CONSTRAINT permissions_users_permissions_fk FOREIGN KEY (permission_id) REFERENCES public.permissions(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT permissions_users_users_fk FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
